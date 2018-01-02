d69c8d5bb0c35af0b2886171bec0cedb37454330
unknown <>
2018-01-02 10:59:53 +0800

    测试版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,155 @@
-//index.js
-//获取应用实例
-const app = getApp()
+'use strict';
+let choose_year = null,
+	choose_month = null;
+const conf = {
+	data: {
+		hasEmptyGrid: false,
+		showPicker: false
+	},
+	onLoad() {
+		const date = new Date();
+		const cur_year = date.getFullYear();
+		const cur_month = date.getMonth() + 1;
+		const weeks_ch = [ '日', '一', '二', '三', '四', '五', '六' ];
+		this.calculateEmptyGrids(cur_year, cur_month);
+		this.calculateDays(cur_year, cur_month);
+		this.setData({
+			cur_year,
+			cur_month,
+			weeks_ch
+		});
+	},
+	getThisMonthDays(year, month) {
+		return new Date(year, month, 0).getDate();
+	},
+	getFirstDayOfWeek(year, month) {
+		return new Date(Date.UTC(year, month - 1, 1)).getDay();
+	},
+	calculateEmptyGrids(year, month) {
+		const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+		let empytGrids = [];
+		if (firstDayOfWeek > 0) {
+			for (let i = 0; i < firstDayOfWeek; i++) {
+				empytGrids.push(i);
+			}
+			this.setData({
+				hasEmptyGrid: true,
+				empytGrids
+			});
+		} else {
+			this.setData({
+				hasEmptyGrid: false,
+				empytGrids: []
+			});
+		}
+	},
+	calculateDays(year, month) {
+		let days = [];
 
-Page({
-  data: {
-    motto: 'Hello 日历',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+		const thisMonthDays = this.getThisMonthDays(year, month);
+
+		for (let i = 1; i <= thisMonthDays; i++) {
+			days.push({
+				day: i,
+				choosed: false
+			});
+		}
+
+		this.setData({
+			days
+		});
+	},
+	handleCalendar(e) {
+		const handle = e.currentTarget.dataset.handle;
+		const cur_year = this.data.cur_year;
+		const cur_month = this.data.cur_month;
+		if (handle === 'prev') {
+			let newMonth = cur_month - 1;
+			let newYear = cur_year;
+			if (newMonth < 1) {
+				newYear = cur_year - 1;
+				newMonth = 12;
+			}
+
+			this.calculateDays(newYear, newMonth);
+			this.calculateEmptyGrids(newYear, newMonth);
+
+			this.setData({
+				cur_year: newYear,
+				cur_month: newMonth
+			});
+
+		} else {
+			let newMonth = cur_month + 1;
+			let newYear = cur_year;
+			if (newMonth > 12) {
+				newYear = cur_year + 1;
+				newMonth = 1;
+			}
+
+			this.calculateDays(newYear, newMonth);
+			this.calculateEmptyGrids(newYear, newMonth);
+
+			this.setData({
+				cur_year: newYear,
+				cur_month: newMonth
+			});
+		}
+	},
+	tapDayItem(e) {
+		const idx = e.currentTarget.dataset.idx;
+		const days = this.data.days;
+		days[ idx ].choosed = !days[ idx ].choosed;
+		this.setData({
+			days,
+		});
+	},
+	chooseYearAndMonth() {
+		const cur_year = this.data.cur_year;
+		const cur_month = this.data.cur_month;
+		let picker_year = [],
+			picker_month = [];
+		for (let i = 1900; i <= 2100; i++) {
+			picker_year.push(i);
+		}
+		for (let i = 1; i <= 12; i++) {
+			picker_month.push(i);
+		}
+		const idx_year = picker_year.indexOf(cur_year);
+		const idx_month = picker_month.indexOf(cur_month);
+		this.setData({
+			picker_value: [ idx_year, idx_month ],
+			picker_year,
+			picker_month,
+			showPicker: true,
+		});
+	},
+	pickerChange(e) {
+		const val = e.detail.value;
+		choose_year = this.data.picker_year[val[0]];
+		choose_month = this.data.picker_month[val[1]];
+	},
+	tapPickerBtn(e) {
+		const type = e.currentTarget.dataset.type;
+		const o = {
+			showPicker: false,
+		};
+		if (type === 'confirm') {
+			o.cur_year = choose_year;
+			o.cur_month = choose_month;
+			this.calculateEmptyGrids(choose_year, choose_month);
+			this.calculateDays(choose_year, choose_month);
+		}
+		
+		this.setData(o);
+	},
+	onShareAppMessage() {
+		return {
+			title: '小程序日历',
+			desc: '还是新鲜的日历哟',
+			path: 'pages/index/index'
+		};
+	}
+};
+
+Page(conf);
