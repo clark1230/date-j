e7b7c1235a20c319ec5b74d0806006459f486bdc
unknown <>
2018-01-02 10:54:36 +0800

    刘上的修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello XX日历',
+    motto: 'Hello 日历',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
